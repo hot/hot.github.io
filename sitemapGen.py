044bf4a85a9aa48cc528fcb3078d34879a4effe0
yjl <y@y>
2015-05-19 17:58:17 +0800

    add sitemap

---
+++ sitemapGen.py
@@ -0,0 +1,24 @@
+import json
+f = open("main.json")
+jData = json.load(f)
+f.close()
+
+xmlHead = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
+"""
+
+xmlEntry = """ <url><loc>http://hot.github.io/{}</loc></url>
+"""
+xmlEnd = """</urlset>"""
+
+wholeXml = xmlHead
+
+for post in jData["posts"]:
+    wholeXml += xmlEntry.format(post["path"])
+
+wholeXml += xmlEnd
+
+with open('sitemap.xml', 'w') as outfile:
+    outfile.write(wholeXml)
+
+
