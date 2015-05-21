6ba36d445cb77515d6a5e721eb6e046f6142b0c1
yjl <y@y>
2015-05-21 11:44:07 +0800

    add urlGen

---
+++ urlGen.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import json
+f = open("main.json")
+jData = json.load(f)
+f.close()
+
+xmlHead = """
+<html>
+<body>
+"""
+
+xmlEntry = """ <a href="{}">{}</a>
+"""
+xmlEnd = """</body></html>"""
+
+wholeXml = xmlHead
+
+for post in jData["posts"]:
+    path = post["path"]
+    wholeXml += xmlEntry.format(path, path)
+
+wholeXml += xmlEnd
+
+with open('alllinks.html', 'w') as outfile:
+    outfile.write(wholeXml)
+
+
